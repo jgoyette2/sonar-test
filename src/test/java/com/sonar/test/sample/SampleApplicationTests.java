dfef77cd20729a69c236bd389fd79961fcc8fefd
John Goyette <jgoyette@archy.com>
2024-10-11 14:51:32 -0400

    Added sample project

---
+++ src/test/java/com/sonar/test/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonar.test.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
